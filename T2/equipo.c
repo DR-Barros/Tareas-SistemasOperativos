8a319682d16beae0a86a7bd18155842305edfd67
Daniel <danielradriganb@gmail.com>
2023-09-02 18:43:50 -0400

    Tarea 2 finalizada con detalles por mejorar

--- T2/equipo.c
+++ T2/equipo.c
@@ -8,15 +8,19 @@
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 int wait = 0, equipos = 0;
-char *equipo[1000000][5];
+char ***equipo;
+
 
 
 char **hay_equipo(char *nombre) {
   pthread_mutex_lock(&m);
   int equipoAct = wait/5;
   int pos = wait%5;
+  if (pos == 0){
+    equipo[equipoAct] = malloc(5* sizeof(char *));
+  }
   equipo[equipoAct][pos] = nombre;
-  if (wait%5 == 4){
+  if (pos == 4){
     wait++;
     equipos++;
     pthread_cond_broadcast(&c);
@@ -27,13 +31,13 @@ char **hay_equipo(char *nombre) {
     }
   }
   pthread_mutex_unlock(&m);
-  char **team = malloc(5*sizeof(char *));
-  team = equipo[equipoAct];
-  return team;
+  return equipo[equipoAct];
 }
 
 void init_equipo(void) {
+  equipo = malloc(1000000 *sizeof(char **));
 }
 
 void end_equipo(void) {
+  free(equipo);
 }
