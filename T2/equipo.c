104bf8d94ac98e6ef6a10363e6ce8514831cd435
Daniel <danielradriganb@gmail.com>
2023-09-05 23:11:44 -0300

    Avance de tarea, no corre para grandes arreglos

--- T2/equipo.c
+++ T2/equipo.c
@@ -8,7 +8,10 @@
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 int wait = 0, equipos = 0;
-char ***equipo;
+//Buffer de equipos
+char **equipo;
+int actual = 0;
+int estado = 0, eFaltante =5;
 
 
 
@@ -16,26 +19,44 @@ char **hay_equipo(char *nombre) {
   pthread_mutex_lock(&m);
   int equipoAct = wait/5;
   int pos = wait%5;
+  int my_num = wait++;
+  // pasan en orden 
+  while (my_num != actual && equipoAct != equipos){
+    pthread_cond_wait(&c, &m);
+  }
+  //el primero crea el arreglo
   if (pos == 0){
-    equipo[equipoAct] = malloc(5* sizeof(char *));
+    char **team = malloc(5*sizeof(char *));
+    equipo = team;
+    estado = 0;
+    eFaltante = 5;
   }
-  equipo[equipoAct][pos] = nombre;
-  if (pos == 4){
-    wait++;
-    equipos++;
-    pthread_cond_broadcast(&c);
-  } else{
-    wait++;
-    while (equipoAct == equipos){
-      pthread_cond_wait(&c, &m);
-    }
-  }
+  //se van agregando al arreglo
+  equipo[pos] = nombre;
+  actual++;
+  pthread_cond_broadcast(&c);
+  char **equipo_local = equipo;
+  estado++;
   pthread_mutex_unlock(&m);
-  return equipo[equipoAct];
+  //chequear que avance
+  printf("En el jugador: %d \n", my_num);
+  pthread_mutex_lock(&m);
+  //espera a que todos los jugadores se hayan agregado al equipos para hacer el return
+  while (estado < 5){
+    pthread_cond_wait(&c, &m);
+  }
+  eFaltante--;
+  //si ya estan listo todos los equipos permite la entrada del resto
+  if (eFaltante == 0){
+    equipos++;
+  }
+  pthread_cond_broadcast(&c);
+  pthread_mutex_unlock(&m);
+  return equipo_local;
 }
 
 void init_equipo(void) {
-  equipo = malloc(1000000 *sizeof(char **));
+  equipo = malloc(sizeof(char **));
 }
 
 void end_equipo(void) {
